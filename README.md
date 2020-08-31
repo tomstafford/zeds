142cc45008295f4fd65f471feb45c88756d4f41a
tomstafford <tom@idiolect.org.uk>
2020-08-31 17:10:44 +0100

    add README description

---
+++ README.md
@@ -0,0 +1,12 @@
+python3 script for finding/loading zed's pages. Speeds up your weekly shop
+
+looks in the file shopping.csv for items and either searches for them or, if there is a url, loads that page
+
+you can then click "add to basket"
+
+conda environment environment.yml will ensure you have the right library versions etc but it doesn't rely on anything fancy so it should work ok without as long as you have python 3
+
+Not licensed, endorsed, the responsibility of, etc Zeds in any way!
+
+Do it the old fashioned way at
+https://store.zedswholefoods.co.uk/
--- zeds.py
+++ zeds.py
@@ -5,13 +5,13 @@ Created on 31 Aug 2020
 
 @author: tomstafford
 
-python3 script for finding/loading zeds pages
+python3 script for finding/loading zed's pages. Speeds up your weekly shop
 
 looks in the file shopping.csv for items and either searches for them or, if there is a url, loads that page
 
 you can then click "add to basket"
 
-conda environment environment.yml will ensure you have the right library versions etc but it doesn't rely on anything fancy so it should work ok
+conda environment environment.yml will ensure you have the right library versions etc but it doesn't rely on anything fancy so it should work ok without as long as you have python 3
 
 Not licensed, endorsed, the responsibility of, etc Zeds in any way!
 
